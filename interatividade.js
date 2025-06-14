6ef812e33ac8efd13a4329a476405be77a5e74bb
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-06-14 11:54:04 -0300

    strings

---
+++ interatividade.js
@@ -0,0 +1,6 @@
+let usuario = prompt("Digite o seu nome: ");
+
+// método para dar uma mensagem na tela do usuário
+alert(`${usuario}, seja bem-vindo`);
+
+console.log(usuario);
---
+++ strings/string.js
@@ -0,0 +1,15 @@
+// Como declarar uma variável do tipo string:
+
+let string = "alguma coisa";
+
+// Concatenar (juntar um texto no outro);
+
+let msg1 = "Olá, ";
+let msg2 = "Seja Bem-Vindo(a)";
+let concatenar = msg1 + msg2;
+
+let pessoa = "Gleyson";
+let msg3 = pessoa + " Olá, seja brm-vindo"
+
+// método para visualizar o que contém dentro de uma variável
+console.log(msg3);
