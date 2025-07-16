8accd7f8b9c7e9de9bd42fe7fdaa26b0f9ef0525
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-07-16 16:37:50 -0300

    criando e modificando arrays

---
+++ arrays/explorando_arrays.js
@@ -0,0 +1,20 @@
+// Criando uma lista em array de strings:
+let cargos = ["Médico", "Admin", "Advogado", "Analista de Sistemas"];
+
+// Selecionando um item dentro da lista de array de string:
+// cargos[3];
+
+// Alterando o valor dinamicamente:
+// cargos[2] = "Contador";
+
+// Adicionando um item na lista:
+// cargos.push("Devops");
+
+// Removendo o último item da lista:
+// cargos.pop();
+
+// Removendo um item específico da lista:
+cargos.splice(2, 1);
+
+// Visualizando no console:
+console.log(cargos);
