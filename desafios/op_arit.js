9d0061910408d188e9f7ca318bc2b48429edf9a5
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-07-16 17:39:37 -0300

    operadores aritméticos

---
+++ desafios/op_arit.js
@@ -0,0 +1,8 @@
+let cxPirulito = 2;
+let qtdPorCx = 20;
+let totalAmigos = 5;
+let totalPirulitos = cxPirulito * qtdPorCx;
+
+let resultadoPorAmigo = totalPirulitos/totalAmigos;
+
+console.log(resultadoPorAmigo);
