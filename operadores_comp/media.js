8fce6940a5e71367a84a80842e30bb5880c3281e
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-07-17 18:51:06 -0300

    aprendendo operadores em JS

---
+++ operadores_comp/media.js
@@ -0,0 +1,7 @@
+let nota1 = Number(prompt("Digite o valor da N1:"));
+let nota2 = Number(prompt("Digite o valor da N2:"));
+let nota3 = Number(prompt("Digite o valor da N3:"));
+
+let media = (nota1 + nota2 + nota3);
+
+alert(media >= 5);
---
+++ operadores_comp/operadores.js
@@ -0,0 +1,18 @@
+// Comparador de igualdade:
+a === b 
+
+// Comparador de diferença:
+a !== b
+
+// Comparador maior:
+a > b
+
+// Comparador menor:
+a < b
+
+// Comparador maior igual:
+a >= b
+
+// Comparador menor igual:
+a <= b
+
