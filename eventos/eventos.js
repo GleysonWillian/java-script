c77ddc9cb98c9c192600e6a8d1ba18ae2283eb4e
Gleyson Willian Bruno Costa Rocha <gleysonwillian14@gmail.com>
2025-07-18 14:54:30 -0300

    evento em JS

---
+++ eventos/eventos.js
@@ -0,0 +1,8 @@
+// // Capturando o "meuBotao" e evento click do botão inserido no HTML e realizando a ação após o click:
+document.getElementById("meuBotao").addEventListener("click", function(){
+    alert("O botão foi clicado!")
+})
+
+// function botaoClicado() {
+//     alert("Botão clicado!")
+// }
